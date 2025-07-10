7d9b715d8daf6067a91eb925dcd89eeb32cea984
nzanni <nzanni98@gmail.com>
2025-07-10 20:24:57 -0300

    git ignore + clase alumno + clase carrera

---
+++ Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿namespace SistemaAcademicoZanni.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public int DNI { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime FechaDeNacimiento { get; set; }
+    }
+}
