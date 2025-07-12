df8e62fb64df1229c8045ee79661a8ecdf57fb03
nzanni <nzanni98@gmail.com>
2025-07-11 21:45:50 -0300

    Listo Eliminar carrera/alumno

--- Pages/Alumnos/DeleteAlumno.cshtml.cs
+++ Pages/Alumnos/DeleteAlumno.cshtml.cs
@@ -1,12 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademicoZanni.Models;
+using static SistemaAcademicoZanni.Data.DatosCompartidosModel;
 
 namespace SistemaAcademicoZanni.Pages.Alumnos
 {
     public class DeleteAlumnoModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Alumno Alumno { get; set; }
+
+        public void OnGet(int id)
         {
+
+            foreach (var c in DatosCompartidos.Alumnos)
+            {
+                if (c.Id == id)
+                {
+                    Alumno = c;
+                    break;
+                }
+            }
         }
+        public IActionResult OnPost()
+        {
+            Alumno alumnoAEliminar = null;
+
+            foreach (var c in DatosCompartidos.Alumnos)
+            {
+                if (c.Id == Alumno.Id)
+                {
+                    alumnoAEliminar = c;
+                    break;
+                }
+            }
+
+            if (alumnoAEliminar != null)
+            {
+                DatosCompartidos.Alumnos.Remove(alumnoAEliminar);
+            }
+
+            return RedirectToPage("IndexAlumno");
+        }
+
     }
 }
--- Pages/Carreras/DeleteCarrera.cshtml.cs
+++ Pages/Carreras/DeleteCarrera.cshtml.cs
@@ -1,12 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademicoZanni.Models;
+using static SistemaAcademicoZanni.Data.DatosCompartidosModel;
 
 namespace SistemaAcademicoZanni.Pages.Carreras
 {
     public class DeleteCarreraModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Carrera Carrera { get; set; }
+
+        public void OnGet(int id)
         {
+
+            foreach (var c in DatosCompartidos.Carreras)
+            {
+                if (c.Id == id)
+                {
+                    Carrera = c;
+                    break;
+                }
+            }
         }
+        public IActionResult OnPost()
+        {
+            Carrera carreraAEliminar = null;
+
+            foreach (var c in DatosCompartidos.Carreras)
+            {
+                if (c.Id == Carrera.Id)
+                {
+                    carreraAEliminar = c;
+                    break;
+                }
+            }
+
+            if (carreraAEliminar != null)
+            {
+                DatosCompartidos.Carreras.Remove(carreraAEliminar);
+            }
+
+            return RedirectToPage("IndexCarrera");
+            }
+
     }
 }
